511a669312a6176af18fe809e2132bf2f3045c52
HiramMcc <hirammisael.cerdacasas@gmail.com>
2023-11-20 21:26:37 -0600

    Commit
    intento de solución de import {angular fireModule}

--- SupremeSavor/src/app/app.module.ts
+++ SupremeSavor/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+//  import {AngularFireModule} from '@angular/fire';
+import { environment } from '../environments/environment';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [AppComponent],
